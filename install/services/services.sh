80d246769dc94bde81dc7c74f6f7ce036d817778
joaquin <joaquin@jtekk.dev>
2026-02-17 21:27:05 -0700

    .

--- install/services/services.sh
+++ install/services/services.sh
@@ -2,8 +2,8 @@
 set -e
 
 if [ "$(id -u)" -eq 0 ]; then
-	echo "Please do not run as root!"
-	exit 1
+  echo "Please do not run as root!"
+  exit 1
 fi
 
 doas usermod -aG wheel,input,plugdev,users jtekk
@@ -21,18 +21,18 @@ doas apk add '!pipewire-dinit-link' '!wireplumber-dinit-link'
 # add greetd whenever ready
 SERVICES="iwd dhcpcd"
 for SERVICE in $SERVICES; do
-	if doas dinitctl status $SERVICE >/dev/null 2>&1; then
-		echo "$SERVICE was already enabled"
-	else
-		doas dinitctl enable $SERVICE
-	fi
+  if doas dinitctl status $SERVICE >/dev/null 2>&1; then
+    echo "$SERVICE was already enabled"
+  else
+    doas dinitctl enable $SERVICE
+  fi
 done
 
-USER_SERVICES="kanshi pipewire pipewire-pulse pipewire waybar swayosd-server swayosd-libinput-backend mako swayidle xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr"
+USER_SERVICES="kanshi pipewire pipewire-pulse pipewire waybar swayosd-server swayosd-libinput-backend mako swayidle "
 for US in $USER_SERVICES; do
-	if dinitctl status $US >/dev/null 2>&1; then
-		echo "$US was already enabled..."
-	else
-		dinitctl enable $US
-	fi
+  if dinitctl status $US >/dev/null 2>&1; then
+    echo "$US was already enabled..."
+  else
+    dinitctl enable $US
+  fi
 done
